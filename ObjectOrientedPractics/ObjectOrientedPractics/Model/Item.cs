f0bcd13f1cd7487afa0052cf603fa54fd86be791
inittose <pol.konnik@gmail.com>
2024-02-11 12:44:46 +0700

    Реализовал класс Item

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,128 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о товаре.
+    /// </summary>
+    internal class Item
+    {
+        /// <summary>
+        /// Уникальный идентификатор данного товара.
+        /// </summary>
+        private readonly int _id = IdGenerator.GetNextId();
+
+        /// <summary>
+        /// Именование товара.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Описание товара.
+        /// </summary>
+        private string _info;
+
+        /// <summary>
+        /// Стоимость товара.
+        /// </summary>
+        private float _cost;
+
+        /// <summary>
+        /// Возвращает уникальный идентификатор данного товара.
+        /// </summary>
+        public int Id 
+        { 
+            get 
+            { 
+                return _id; 
+            } 
+        }
+
+        /// <summary>
+        /// Возвращает и задает наименование товара. 
+        /// Должно состоять не более чем из 200 символов.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length > 200)
+                {
+                    throw new ArgumentException("Name cannot exceed 200 characters.");
+                }
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает описание товара. 
+        /// Должно состоять не более чем из 1000 символов.
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                return _info;
+            }
+            set
+            {
+                if (value.Length > 1000) 
+                {
+                    throw new ArgumentException("Info cannot exceed 1000 characters.");
+                }
+                _info = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает стоимость товара. 
+        /// Должна входить в диапозон: от 0 до 100 000.
+        /// </summary>
+        public float Cost
+        {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                if (value < 0 || value > 100000f)
+                {
+                    throw new ArgumentException("Cost is not included in the range from 0 to 100 000.");
+                }
+                _cost = value;
+            }
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Item"/>.
+        /// </summary>
+        /// <param name="name">
+        ///     Именование товара. 
+        ///     Должно состоять не более чем из 200 символов.
+        /// </param>
+        /// <param name="info">
+        ///     Описание товара.
+        ///     Должно состоять не более чем из 1000 символов.
+        /// </param>
+        /// <param name="cost">
+        ///     Стоимость товара.
+        ///     Должна входить в диапозон: от 0 до 100 000.
+        ///</param>
+        public Item(string name, string info, float cost)
+        {
+            Name = name;
+            Info = info;
+            Cost = cost;
+        }
+    }
+}
